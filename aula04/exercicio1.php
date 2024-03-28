44fbe87d8109a50a68809ef839055d8dfca9900e
Isabelle Almeida <isabelle.ma.aquino@gmail.com>
2024-03-28 20:44:46 -0300

    aulafunction

---
+++ aula04/exercicio1.php
@@ -0,0 +1,15 @@
+<?php
+     function soma ($x, $y) {
+        $s = $x + $y;
+        return $s;
+     } 
+
+     echo "2 + 2 =" . soma(11, 11);
+     echo "<br>";
+
+     function somaJoao($x, $y){
+        echo $x . " + " . $y . " = " . $x + $y;
+     }
+
+     somaJoao(6, 9);
+?>
