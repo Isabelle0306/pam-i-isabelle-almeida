9849e53a7e389dea01099f6fe9a33973f2605a38
Isabelle Almeida <isabelle.ma.aquino@gmail.com>
2024-04-04 22:28:15 -0300

    aula05

---
+++ aula05/formulario.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+
+    <form action= "resultado.php" method="POST">
+        <label for="nome">Nome:</label><input type="text" name="nome"><br>
+        <label for="email">Email:</label><input type="text" name="email"><br>
+        <input type="subimit">
+    </form>
+    </body>
+ 
+
+<html>
